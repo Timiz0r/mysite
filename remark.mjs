8bb7a464581e503853dee0ff69f2c841c7fabfff
Timiz0r <5377118+Timiz0r@users.noreply.github.com>
2025-02-03 05:08:43 -0800

    Fix exception coming from modified date of uncommitted files.
    Also standardized to UTC while in the area.

--- remark.mjs
+++ remark.mjs
@@ -1,5 +1,8 @@
 import { execSync } from "child_process";
+import dayjs from "dayjs";
+import utc from 'dayjs/plugin/utc';
 
+dayjs.extend(utc);
 
 // Originally tried providing dayjs, then Date. Everything ends up being a string, so no point.
 // Also why remarked-formatter.ts is a thing.
@@ -8,7 +11,8 @@ export function remarkModifiedTime() {
     const filepath = file.history[0];
     // TODO: would prefer not running many sync commands, so find a way to build a map
     // not high pri unless there are many posts, though.
-    const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
-    file.data.astro.frontmatter.lastModified = result.toString();
+    const timestamp = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
+    const time = dayjs(timestamp);
+    file.data.astro.frontmatter.lastModified = time.utc().format("YYYY-MM-DDTHH:mm:ss.sssZ");
   };
 }
--- src/remarked-frontmatter.ts
+++ src/remarked-frontmatter.ts
@@ -1,6 +1,12 @@
 import dayjs from "dayjs";
 
 export function lastModified(r: Record<string, any>): Date {
+    const timestamp = r.lastModified;
+
+    // happens when file not committed yet -- aka only during local testing
+    if (timestamp == null) {
+        return new Date();
+    }
     return dayjs(r.lastModified).toDate();
 }
 
