d5b5ad562ab7c98bc804e46f8e1f73747d0d8bf9
Timiz0r <5377118+Timiz0r@users.noreply.github.com>
2025-02-03 07:01:38 -0800

    Add tailwind v3 and shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,19 @@ import sitemap from '@astrojs/sitemap';
 import { remarkModifiedTime } from './remark.mjs';
 
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kyouha.today',
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    })
+  ],
 
   markdown: {
       remarkPlugins: [remarkModifiedTime],
