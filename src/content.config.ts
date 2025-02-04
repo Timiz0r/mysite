4cb8be546878aa723f1cca29ce8c5f36c307d31b
Timiz0r <5377118+Timiz0r@users.noreply.github.com>
2025-02-04 06:07:35 -0800

    Add way to specify specific blog post id.
    Turns out a lot of the work wasn't needed. First, index.* works as one would expect. Furthermore, there aren't a lot of scenarios where providing an id override is useful, since the filename should generally suffice.

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,3 +1,5 @@
+import path from "node:path";
+import { slug as githubSlug } from 'github-slugger'
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
@@ -10,19 +12,39 @@ import { defineCollection, z } from 'astro:content';
 */
 
 const blog = defineCollection({
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+	loader: glob(
+		{
+			base: './src/content/blog',
+			pattern: '**/*.{md,mdx}',
+			generateId: ({ entry, data }) => {
+				let providedId = <string>data.id;
+				if (providedId) {
+					entry = path.join(path.dirname(entry), providedId);
+				}
+
+				const id = generateId(entry);
+				return id;
+			}
+		}),
 	schema: ({ image }) => z.object({
 		title: z.string(),
 		description: z.string(),
 		pubDate: z.coerce.date(),
 		cover: image(), // alt text is effectively the description field
-
-		// instead of the full path being the id, we drop the filenam
-		// useful for when there are images in the same folder, but the folder only has one post
-		single: z.boolean().default(true),
-		// or, instead of the full path or "single path", we use "{dir}/id/"
+		// instead of the full path or "single path", we use "{dir}/id/"
 		id: z.string().optional(),
 	}),
 });
 
 export const collections = { blog };
+
+// is a more concise implementation of the default id generator
+function generateId(entry: string): string {
+	const p = path.parse(entry);
+	entry = path.join(p.dir, p.name);
+	const segments = entry.split(path.sep);
+	if (segments.at(-1) == 'index') segments.pop();
+
+	const slug = segments.map(s => githubSlug(s)).join('/');
+	return slug;
+}
